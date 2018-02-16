35e6787322fd9a701bc89451046989e0662e05be
Dylan Cetrulo <dylancet@gmail.com>
2018-02-16 13:23:37 -0700

    update site layout

---
+++ states.js
@@ -0,0 +1,17 @@
+var pages = ["about", "design", "coding", "contact"];
+var colors = ["#f86869", "#669766", "#f5e066", "#66a2d7"];
+function show(id) {
+    for (var i = 0; i < pages.length; i++) {
+        if (pages[i] == id) {
+            $('path').css({
+               fill: colors[i] 
+            });
+            $('#' + id + "-page").css({
+                display: 'block'
+            });
+            $('#' + id + "-page").animate({left:"8px", right:"8px"}, 500);
+        }else if($('#' + pages[i] + "-page").css('display') == 'block'){
+            $('#' + pages[i] + "-page").animate({left:"-105%", right:"105%"}, 500, function(){$(this).css({display: 'none'});});
+        }
+    }
+}
